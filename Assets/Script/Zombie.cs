0c0e87fe9f1c4f93d050a4e87d2e89c319a4afc4
matioka <ayaka0308horn@gmail.com>
2020-12-05 16:12:16 +0900

    プロジェクト作成

---
+++ Assets/Script/Zombie.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Zombie : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+       
+
+    }
+}
+/*public int hitPoint = 100;  //HP
+ //HPが0になったときに敵を破壊する
+        if (hitPoint <= 0)
+        {
+            Destroy(gameObject);
+        }
+ //ダメージを受け取ってHPを減らす関数
+    public void Damage(int damage)
+{
+    //受け取ったダメージ分HPを減らす
+    hitPoint -= damage;
+}*/
---
+++ Assets/Script/ballet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballet : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + transform.forward;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
