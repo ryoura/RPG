c12cd4b1b3bd3d8bfc0d53a0fea429518a6bddba
matioka <ayaka0308horn@gmail.com>
2021-01-30 17:13:05 +0900

    プレイヤー

---
+++ Assets/Script/Script/Character/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Character : MonoBehaviour
+{
+    protected NavMeshAgent SelfNavmeshAgent { get; private set; }
+
+    [SerializeField]
+    float moveSpeed = 5.0f;
+
+    protected void Move(Vector3 axis)
+    {
+        SelfNavmeshAgent = GetComponent<NavMeshAgent>();
+        SelfNavmeshAgent.Move(axis * moveSpeed * Time.deltaTime);
+    }
+}
